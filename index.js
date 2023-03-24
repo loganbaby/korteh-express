bdd2307dd1132543489210084e085154a6e77bf7
loganbaby <shifo3456@gmail.com>
2023-03-24 14:39:02 +0300

    adaptive fix

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const app = express()
+const app = express();
 
 const urlencodedParser = bodyParser.urlencoded({
     extended: false,
-})
+});
 
-const port = 3000
+const port = 3000;
 
-app.set('views', path.resolve('.', 'views'))
-app.set("view engine", "ejs")
-console.log('path to /views: ' + path.resolve('.', 'views/'))
+app.set('views', path.resolve('.', 'views'));
+app.set("view engine", "ejs");
+console.log('path to /views: ' + path.resolve('.', 'views/'));
 
-app.use(express.static(path.resolve('.', 'public')))
-console.log('path to /public: ' + path.resolve('.', 'public/'))
+app.use(express.static(path.resolve('.', 'public')));
+console.log('path to /public: ' + path.resolve('.', 'public/'));
 
 app.get('/page', urlencodedParser, function (request, response) {
-    console.log('got -> /page')
-})
+    console.log('got -> /page');
+});
 
 app.post('/page', urlencodedParser, function (request, response) {
-    if (!request.body) return response.sendStatus(400)
-    console.log("%j", request.body)        // cannot convert to primitive type Object
+    if (!request.body) return response.sendStatus(400);
+    console.log("%j", request.body);        // cannot convert to primitive type Object
     
-    response.redirect('/')
-})
+    response.redirect('/');
+});
 
 app.use('/', function (request, response, next) {
     response.render('index', {
         title: 'Студенческий портал demo'
-    })
-})
+    });
+});
 
-app.listen(port, () => console.log(`Server started... on port ${port}`))
+app.listen(port, () => console.log(`Server started... on port ${port}`));
