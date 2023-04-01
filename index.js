0e7010f6e9e71784a1dc21305886abfdebdb4d09
loganbaby <shifo3456@gmail.com>
2023-04-01 23:40:36 +0300

    added new page + postgesql

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+'use strict'
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+
+const pgp = require("pg-promise")(/*options*/);
+const db = pgp('postgres://logbaby:666@localhost:3000/korteh-project');     // path to your postgreSQL database
 
 const app = express();
 
@@ -17,15 +22,23 @@ console.log('path to /views: ' + path.resolve('.', 'views/'));
 app.use(express.static(path.resolve('.', 'public')));
 console.log('path to /public: ' + path.resolve('.', 'public/'));
 
-app.get('/page', urlencodedParser, function (request, response) {
-    console.log('got -> /page');
+app.get('/send-email-response', urlencodedParser, function (request, response) {
+    console.log('got -> /send-email-response');
 });
 
-app.post('/page', urlencodedParser, function (request, response) {
+app.post('/send-email-response', urlencodedParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     console.log("%j", request.body);        // cannot convert to primitive type Object
     
     response.redirect('/');
+});
+
+app.get('/login', urlencodedParser, function (request, response) {
+    console.log('got -> /login');
+
+    response.render('login', {
+        title: 'Login page demo'
+    });
 });
 
 app.use('/', function (request, response, next) {
