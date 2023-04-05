7e71eaf0f277ed50a7227b37f7df99f338015ba7
loganbaby <shifo3456@gmail.com>
2023-04-05 18:06:39 +0300

    added chat interface + rooms adapted (users p2p like)

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -3,7 +3,6 @@ const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
-// парсинг get параметров
 const {username, room} = Qs.parse(location.search, {
     ignoreQueryPrefix: true
 });
@@ -22,23 +21,18 @@ function outputMessage(message) {
     document.querySelector('.chat-messages').appendChild(div);
 }
 
-// отрисовка имени комнаты в DOM
 function outputRoom(room) {
     roomName.innerText = room;
 }
 
-// отрисовка активных юезров в комнате в DOM
 function outputUsers(users) {
     userList.innerHTML = `${users.map(user => `<li>${user.username}</li>`).join('')}`;
 }
 
-// инициализация сокета
 const socket = io();
 
-// ответ при соединении с комнатой
 socket.emit('joinRoom', {username, room});
 
-// listener roomUsers
 socket.on('roomUsers', ({room, users}) => {
     outputRoom(room);
     outputUsers(users);
@@ -46,7 +40,6 @@ socket.on('roomUsers', ({room, users}) => {
     console.log('outputting')
 });
 
-// listener message - сообщение от сервера
 socket.on('message', message => {
     console.log(message);
     outputMessage(message);
@@ -54,13 +47,10 @@ socket.on('message', message => {
     chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
-// отправка формы
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const msg = e.target.elements.msg.value;
-
-    // отправка на сервер
     socket.emit('chatMessage', msg);
 
     e.target.elements.msg.value = '';
